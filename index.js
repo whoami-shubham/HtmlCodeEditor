c697b9383eacb052dbe0c9fe96d30e2ff879fb27
shubham <28999685+whoami-shubham@users.noreply.github.com>
2019-03-28 16:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+/*
+*  here event listener is listening for keyup event and on keyup event call display function.
+*  and display function is fetching  contents of all three sections and save that content into html,css and js variable
+*  after that create a <iframe> object assign it to output variable and replace content of iframe. 
+*/
 document.getElementById('code').addEventListener("keyup", display)
 function display(){
-    var html = document.getElementById('html')
-    var css = document.getElementById('css')
-    var js = document.getElementById('js')
+    var html =   document.getElementById('html')
+    var css  =   document.getElementById('css')
+    var js   =   document.getElementById('js')
     var output = document.getElementById('output').contentWindow.document
     output.open()
     output.writeln(
